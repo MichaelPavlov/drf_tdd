28a36f9b2454cd88a2b8e5a87b8374d0de36eb51
Michael Pavlov <mihpavlov@gmail.com>
2017-05-06 17:28:46 +0300

    adds Puppy model

--- puppies/models.py
+++ puppies/models.py
@@ -1,3 +1,20 @@
-from django.db import models
+from django.db.models import Model, CharField, IntegerField, DateTimeField
 
-# Create your models here.
+
+class Puppy(Model):
+    """
+    Puppy model
+    Defines the attributes of a puppy
+    """
+    name = CharField(max_length=255)
+    age = IntegerField()
+    breed = CharField(max_length=255)
+    color = CharField(max_length=255)
+    created_at = DateTimeField(auto_now_add=True)
+    updated_at = DateTimeField(auto_now=True)
+
+    def get_breed(self):
+        return self.name + ' belongs to ' + self.breed + ' breed.'
+
+    def __str__(self):
+        return self.name + ' is added.'
